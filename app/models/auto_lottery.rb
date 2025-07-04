cd906430968c6949e3c1e03165aefef14cbad32a
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-04 12:19:13 +0800

    Update auto_lottery.rb

--- app/models/auto_lottery.rb
+++ app/models/auto_lottery.rb
@@ -4,27 +4,30 @@ class AutoLottery < ActiveRecord::Base
   self.table_name = "auto_lotteries"
 
   belongs_to :post
-  belongs_to :user # 创建者
+  belongs_to :creator, class_name: "User", foreign_key: "user_id"
 
-  serialize :winners, Array
+  has_many :entries, class_name: "AutoLotteryEntry", foreign_key: :auto_lottery_id
 
   validates :post_id, presence: true, uniqueness: true
   validates :prize_name, presence: true
   validates :auto_draw_at, presence: true
+  validates :prize_count, numericality: { greater_than: 0 }
+
+  enum status: { active: 0, drawn: 1 }
 
   scope :active, -> { where("auto_draw_at > ?", Time.current) }
 
   def draw!
     return if drawn?
 
-    entries = AutoLotteryEntry.where(auto_lottery_id: id).where.not(user_id: user_id).distinct(:user_id)
-    user_ids = entries.pluck(:user_id).uniq
-    winners_ids = user_ids.sample(prize_count)
+    # 剔除发起人，去重
+    user_ids = entries.where.not(user_id: creator.id).pluck(:user_id).uniq
+    winner_ids = user_ids.sample(prize_count)
 
     update!(
-      winners: winners_ids,
+      winner_ids: winner_ids,
       drawn_at: Time.current,
-      status: "drawn"
+      status: :drawn
     )
   end
 
